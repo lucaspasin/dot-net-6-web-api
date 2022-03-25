ce1ac1069c778677ae703dbcc453a11bc8755ac9
Lucas Pasin <lucasspasin@gmail.com>
2022-03-25 10:16:46 -0300

    CRUD básico

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/getProduct/{code}", ([FromRoute] string code) =>
+{
+    var product = ProductRepository.GetBy(code);
+    return product;
+});
+
+app.MapPost("/saveProduct", (Product product) =>
+{
+    ProductRepository.Add(product);
+});
+
+app.MapPut("/editEndpoint", (Product product) =>
+{
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+});
+
+app.MapDelete("/deleteProduct/{code}", ([FromRoute] string code) =>
+{
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+});
 
 app.Run();
